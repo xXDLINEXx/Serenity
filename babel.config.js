d05dcf4f587e7e5d30537d0ad5e73f2dace9f2cc
xXDLINEXx <solution1nfopro@gmail.com>
2025-11-03 23:48:40 -0500

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,15 +1,10 @@
 module.exports = function (api) {
   api.cache(true);
+
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo"],
     plugins: [
-      'react-native-reanimated/plugin',
-      // laisse ceci si tu utilises les chemins relatifs type "@/components"
-      ['module-resolver', {
-        alias: {
-          '@': './',
-        },
-      }],
+      "react-native-reanimated/plugin",
     ],
   };
 };
